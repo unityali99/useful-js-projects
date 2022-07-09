9573bc47fecc7fc561854ae432b3324ed9dc26fd
Ali Ahmadi <mrkiller1383@gmail.com>
2022-07-09 22:15:02 +0430

    Added price-range-object excercise.

---
+++ price-range-object/index.js
@@ -0,0 +1,11 @@
+const priceRanges = [
+  {
+    title: "$",
+    rangeName: "inexpensive",
+    minimumRange: 0,
+    maximumRange: 10,
+  },
+  { title: "$$", rangeName: "moderate", minimumRange: 10, maximumRange: 20 },
+  { title: "$$$", rangeName: "pricey", minimumRange: 20, maximumRange: 30 },
+  { title: "$$$$", rangeName: "inexpensive", minimumRange: 30 },
+];
