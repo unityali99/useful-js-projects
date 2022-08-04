035b8f0faef59e11ec861cc10554303067feb7a2
Ali Ahmadi <mrkiller1383@gmail.com>
2022-08-04 15:12:49 +0430

    In oop-stopwatch program, added the methods of stopwatch object to it's prototype and made the properties exposed to the outside.

--- oop-stopwatch/index.js
+++ oop-stopwatch/index.js
@@ -2,25 +2,46 @@ function StopWatch() {
   let duration = 0;
   let started = false;
   let intervalID;
-  this.start = function () {
-    if (started) throw new Error("The stopwatch has already started.");
-    started = true;
-    intervalID = setInterval(() => {
-      ++duration;
-    }, 1000);
-  };
-  this.stop = function () {
-    if (!started) throw new Error("The stopwatch has already stopped.");
-    started = false;
-    clearInterval(intervalID);
-  };
-  this.reset = function () {
-    if (started) throw new Error("Stop the stopwatch in order to reset.");
-    duration = 0;
-  };
+
   Object.defineProperty(this, "duration", {
     get: function () {
       return duration;
     },
+    set: function (value) {
+      duration = value;
+    },
+  });
+  Object.defineProperty(this, "started", {
+    get: function () {
+      return started;
+    },
+    set: function (value) {
+      started = value;
+    },
+  });
+  Object.defineProperty(this, "intervalID", {
+    get: function () {
+      return intervalID;
+    },
+    set: function (value) {
+      intervalID = value;
+    },
   });
 }
+
+StopWatch.prototype.start = function () {
+  if (this.started) throw new Error("The stopwatch has already started.");
+  this.started = true;
+  this.intervalID = setInterval(() => {
+    ++this.duration;
+  }, 1000);
+};
+StopWatch.prototype.stop = function () {
+  if (!this.started) throw new Error("The stopwatch has already stopped.");
+  this.started = false;
+  clearInterval(this.intervalID);
+};
+StopWatch.prototype.reset = function () {
+  if (this.started) throw new Error("Stop the stopwatch in order to reset.");
+  this.duration = 0;
+};
