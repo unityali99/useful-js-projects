4d6ebb8a26a033a0183cc4150cd32855e4e70f05
Ali Ahmadi <mrkiller1383@gmail.com>
2022-08-27 23:18:22 +0430

    es6-stack

---
+++ oop-stack-es6/index.js
@@ -0,0 +1,26 @@
+const privateProp = new WeakMap();
+
+class Stack {
+  constructor(initialStack = []) {
+    if (!Array.isArray(initialStack))
+      throw new Error("Input must be an array.");
+    privateProp.set(this, initialStack);
+  }
+
+  push(item) {
+    privateProp.get(this).push(item);
+  }
+
+  pop() {
+    return privateProp.get(this).pop();
+  }
+
+  peek() {
+    let arr = privateProp.get(this);
+    return arr[arr.length - 1];
+  }
+
+  get count() {
+    return privateProp.get(this).length;
+  }
+}
